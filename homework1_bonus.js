7500472e8de59d413fda6ff6f483653dedf79e39
Zishan Liu <Zishan.liu@outlook.com>
2018-06-04 14:05:10 -0400

    homework1 bonus

---
+++ homework1_bonus.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button')
+
+const changeContents = function(){
+    const heading = document.getElementById("head2")
+    heading.textContent = 'done !'
+}
+
+button.addEventListener('click',changeContents)
