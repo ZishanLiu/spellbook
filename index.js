9fab9bb0c7ff26a7beaa3fcfc29cf5e17fbb3814
Zishan Liu <Zishan.liu@outlook.com>
2018-06-05 09:42:57 -0400

    update a speical paragraph

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const button = document.querySelector('button')
 
 button.addEventListener('click', changeHeading)
 function changeHeading(){
-    document.querySelector('h1').textContent = 'yo'
+    document.querySelector('p.special').textContent = 'yo'
 }
