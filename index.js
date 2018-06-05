0b8983bbca10fcc9f0ded139d6cbe7b7ae07009c
Zishan Liu <Zishan.liu@outlook.com>
2018-06-05 19:42:25 -0400

    homework2 basic

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const form = document.querySelector('form')
 const changeHeading = function(ev){
     ev.preventDefault()
     const f = ev.target
-    const spellName = f.spellName.value
+    const spellName = f.spellName1.value
     document.querySelector('#spells').innerHTML += `<p>` + spellName+`</p>` 
+    document.querySelector('#spells').innerHTML += `<p>` + spellName2 + `</p>`
 
     f.reset()
 }
