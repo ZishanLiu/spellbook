b92f7cea4484ddcea927dfea8c2b8a9ddefc8889
Zishan Liu <Zishan.liu@outlook.com>
2018-06-05 00:15:49 -0400

    homework1_mega_bonus

--- homework1_mega.js
+++ homework1_mega.js
@@ -1,9 +1,10 @@
+/*
 const button = document.getElementById("button")
 
 const changeContents = function(){
     const heading = document.getElementById("heading")
-    const newheading = document.getElementById("input")
+    const newheading = document.getElementById("form")
     heading.textContent = newheading
 }
 
-button.addEventListener('click',changeContents)
+button.addEventListener('click',changeContents)*/
