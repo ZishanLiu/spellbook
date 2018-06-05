0619937e18663f00444dfa210f0c3f5984f84e43
Zishan Liu <Zishan.liu@outlook.com>
2018-06-05 17:43:25 -0400

    homework2_basic

---
+++ homework2_basic.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form')
+
+
+const changeHeading = function(ev){
+    ev.preventDefault()
+    const f = ev.target
+    const spellName = f.spellName1.value
+    const spellName2 = f.spellName2.value
+
+    document.querySelector('#spells').innerHTML += `<p>` + spellName + `</p>` 
+    document.querySelector('#spells').innerHTML += `<p>` + spellName2 + `</p>`
+    
+    f.reset()
+}
+
+form.addEventListener('submit', changeHeading)
