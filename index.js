f1358927af4549a4f483de8d7a4866fe9bcf2b89
Zishan Liu <Zishan.liu@outlook.com>
2018-06-05 20:01:51 -0400

    homework2 basic (2 fields); side notes: hw1 are in separate files in repo

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const changeHeading = function(ev){
     ev.preventDefault()
     const f = ev.target
     const spellName = f.spellName1.value
+    const spellName2 = f.spellName2.value
     document.querySelector('#spells').innerHTML += `<p>` + spellName+`</p>` 
     document.querySelector('#spells').innerHTML += `<p>` + spellName2 + `</p>`
 
     f.reset()
 }
+
 
 form.addEventListener('submit', changeHeading)
