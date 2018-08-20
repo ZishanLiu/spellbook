451ae18ba5da72c155f9eb32fa152d82438377f4
Zishan Liu <Zishan.liu@outlook.com>
2018-08-20 20:19:19 +0100

    homework2 basic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const button = document.querySelector('button')
 
 const changeContents = function(){
@@ -7,3 +8,21 @@ const changeContents = function(){
 
 
 button.addEventListener('click',changeContents)
+=======
+const form = document.querySelector('form')
+
+
+const changeHeading = function(ev){
+    ev.preventDefault()
+    const f = ev.target
+    const spellName = f.spellName1.value
+    const spellName2 = f.spellName2.value
+    document.querySelector('#spells').innerHTML += `<p>` + spellName+`</p>` 
+    document.querySelector('#spells').innerHTML += `<p>` + spellName2 + `</p>`
+
+    f.reset()
+}
+
+
+form.addEventListener('submit', changeHeading)
+>>>>>>> f54be45... homework2 basic
